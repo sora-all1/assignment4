e28ddcb1d3aca078467d0c17feb7a0da66c38756
中村空 <nakamurasora@nakamurasoranoMacBook-Pro.local>
2024-02-16 17:47:33 +0900

    既存のファイルではない場合の対応の追加、追記ではなく編集になるように変更

--- memo.rb
+++ memo.rb
@@ -1,7 +1,6 @@
 require "csv" 
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
-
 memo_type = gets.to_i 
 
 if memo_type === 1
@@ -12,24 +11,28 @@ if memo_type === 1
   puts"完了したらCtrl + Dをおします。"
   imput_memo = STDIN.read.chomp
   
-  CSV.open("#{file_name}.csv" , "w") do |csv|
+  CSV.open("#{file_name}.csv" , "w" ) do |csv|
   csv.puts ["#{imput_memo}"]
-  
   end
   
-  elsif memo_type === 2
+elsif memo_type === 2
   puts "拡張子を除いた既存ファイル名を入力してください。"
   file_name = gets.chomp
   
-  puts"メモしたい内容を記入してください。"
-  puts "Ctrl+Dで保存します。"
+  begin
+  puts CSV.read("#{file_name}.csv")
+  rescue
+  puts "存在しないファイルです。"
+  else
+  puts "編集内容を記入してください。"
+  puts "完了したらCtrl + Dをおします。"
   imput_memo = STDIN.read.chomp
   
-  CSV.open("#{file_name}.csv" , "a") do |csv|
+  CSV.open("#{file_name}.csv" , "w" ) do |csv|
   csv.puts ["#{imput_memo}"]
   end
-  
-  else
-  puts "1か2の数字を入力してください"
   end
 
+else
+  puts "1か2の数字を入力してください"
+end
